146d369dca01a8a9e95c54eac4500474ef63ed0b
Niklas Strand <nikstra@users.noreply.github.com>
2016-07-02 19:34:30 +0200

    Add Home controllers for both Admin and General areas. Add namespace parameter for Default route in RouteConfig.

---
+++ Routing/Areas/Admin/Controllers/HomeController.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Reflection;
+
+namespace Routing.Areas.Admin.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Admin/Home
+        public ActionResult Index()
+        {
+            MethodBase method = MethodBase.GetCurrentMethod();
+            return Content("Action full name: " + method.ReflectedType.FullName + "." + method.Name);
+        }
+    }
+}

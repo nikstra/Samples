1bc90439fbc2307c6c7f7c2b3ad5e0cbe4f54972
Niklas Strand <nikstra@users.noreply.github.com>
2016-07-02 16:55:16 +0200

    Add a route named Date to test route constraints.

--- Routing/App_Start/RouteConfig.cs
+++ Routing/App_Start/RouteConfig.cs
@@ -14,6 +14,13 @@ namespace Routing
             routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
 
             routes.MapRoute(
+                name: "Date",
+                url: "{controller}/{action}/{year}/{month}/{day}",
+                defaults: null,
+                constraints: new { year = @"\d{4}", month = @"\d{2}", day = @"\d{2}" }
+            );
+
+            routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
                 defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }

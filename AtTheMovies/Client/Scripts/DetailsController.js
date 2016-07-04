9ca354d2ebd17008ea3e09179ceff185e97a6ea0
Niklas Strand <nikstra@users.noreply.github.com>
2016-07-04 19:11:00 +0200

    Configure ngRoute, add list and details views and a Details controller.

---
+++ AtTheMovies/Client/Scripts/DetailsController.js
@@ -0,0 +1,15 @@
+﻿(function (app) {
+
+    var DetailsController = function ($scope, $http, $routeParams) {
+
+        var id = $routeParams.id;
+
+        $http.get("/api/movie/" + id)
+            .success(function (data) {
+                $scope.movie = data;
+            });
+    };
+
+    app.controller("DetailsController", DetailsController);
+
+}(angular.module("atTheMovies")));

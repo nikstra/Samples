d7c6af876dcde03382d6b2b19832540e375ab434
Niklas Strand <nikstra@users.noreply.github.com>
2016-07-04 18:27:47 +0200

    Add Angular $http service and call get() to get the movie list from the API controller. Add ng-repeat in Index.cshtml to list movie titles.

--- AtTheMovies/Client/Scripts/ListController.js
+++ AtTheMovies/Client/Scripts/ListController.js
@@ -1,7 +1,14 @@
 ﻿(function (app) {
-    var ListController = function () {
 
+    var ListController = function ($scope, $http) {
+
+        $http.get("/api/movie")
+            .success(function (data) {
+                $scope.movies = data;
+            });
     };
+
+    //ListController.$inject = [$scope, $http];
 
     app.controller("ListController", ListController);
 

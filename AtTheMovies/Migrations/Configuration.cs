eb7e9449f5bb80df0176df6bf6cf435c51487d69
Niklas Strand <nikstra@users.noreply.github.com>
2016-07-04 16:56:37 +0200

    Add db seed.

--- AtTheMovies/Migrations/Configuration.cs
+++ AtTheMovies/Migrations/Configuration.cs
@@ -1,5 +1,6 @@
 namespace AtTheMovies.Migrations
 {
+    using Models;
     using System;
     using System.Data.Entity;
     using System.Data.Entity.Migrations;
@@ -14,18 +15,11 @@ namespace AtTheMovies.Migrations
 
         protected override void Seed(AtTheMovies.Models.MovieDb context)
         {
-            //  This method will be called after migrating to the latest version.
-
-            //  You can use the DbSet<T>.AddOrUpdate() helper extension method 
-            //  to avoid creating duplicate seed data. E.g.
-            //
-            //    context.People.AddOrUpdate(
-            //      p => p.FullName,
-            //      new Person { FullName = "Andrew Peters" },
-            //      new Person { FullName = "Brice Lambson" },
-            //      new Person { FullName = "Rowan Miller" }
-            //    );
-            //
+            context.Movies.AddOrUpdate(m => m.Title,
+                new Movie { Title = "Star Wars", ReleaseYear = 1977, Runtime = 121 },
+                new Movie { Title = "Inception", ReleaseYear = 2010, Runtime = 148 },
+                new Movie { Title = "Toy Story", ReleaseYear = 1995, Runtime = 81 }
+            );
         }
     }
 }
